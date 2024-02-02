6a85c8381add9e9df09d29dcb4bc2a0fd4498527
Anwar Bahou <anwarbahou@fh-b85888.local>
2024-02-02 04:14:13 +0100

    removing old files, fixing bugs

--- ProductDataManipulation/METHODS/filterMethods.js
+++
@@ -1,5 +0,0 @@
-//this function filters products by category
-function filterByCategory(products, category2){
-    const productFilter = products.filter(product => product.category == category2)
-    console.log(productFilter)
-}
--- ProductDataManipulation/api/fetchData.js
+++
@@ -1,7 +0,0 @@
-async function fetchData() {
-    fetch('https://fakestoreapi.com/products')
-    .then((Response) => Response.json())
-    .then((product)=>console.log(product))
-    .catch((error)=>console.log('error :', error.message))
-  }
-  fetchData()
