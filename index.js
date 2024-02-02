fd219b76ef9defeec9a787baf22c5258bf328829
bahou1 <138963406+bahou1@users.noreply.github.com>
2024-02-02 04:38:09 +0100

    adding comments index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const fetchData = require('./fetchData.js');
 const filterByCategory = require('./filterMethods.js');
 const { sortByPriceAsc, sortByPriceDesc } = require('./sortMethods.js');
@@ -9,35 +8,34 @@ const displayProductDetails = require('./productDetails.js');
 const url = 'https://fakestoreapi.com/products';
 
 async function main() {
-  // Fetch data from the API
+  // Fetching Data from the API
   const products = await fetchData(url);
 
-  // Ensure that data is fetched successfully
+  // Testing if the Data has been succesfully captured
   if (!products) {
     console.log('Failed to fetch data from the API. Exiting...');
     return;
   }
 
-  // Filter products by category
+  // Filtering products by category
   const filteredProducts = filterByCategory(products, 'electronics');
 
-  // Sort products by price in ascending order
+  // Sorting  products by price in ascending order
   const sortedAscProducts = sortByPriceAsc(filteredProducts);
 
-  // Sort products by price in descending order
+  // Sorting products by price in descending order
   const sortedDescProducts = sortByPriceDesc(filteredProducts);
 
-  // Search for products by keyword
+  // Searching for products by keyword
   const keywordSearchResult = searchByKeyword(products, 'gaming');
   console.log('Search Result:', keywordSearchResult);
 
-  // Display detailed information for a selected product
+  // Displaying detailed information for a selected product
   const selectedProduct = sortedAscProducts[0];
   displayProductDetails(selectedProduct);
 
-  // Calculate and log the average price of all products
+  // Calculating and log the average price of all products
   const averagePrice = calculateAveragePrice(products);
   console.log('Average Price:', averagePrice);
 }
 
-main();
