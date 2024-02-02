a7c34665066692b3c10f31c484098b4ce2f7827d
Anwar Bahou <anwarbahou@fh-b85888.local>
2024-02-02 04:18:34 +0100

    productDetails.js a function used to show product details (examples are title, description, category..)

---
+++ productDetails.js
@@ -0,0 +1,10 @@
+// productDetails.js
+function displayProductDetails(product) {
+  console.log('Product Details:');
+  console.log('Title:', product.title);
+  console.log('Description:', product.description);
+  console.log('Price:', product.price);
+  // Add other details as needed
+}
+
+module.exports = displayProductDetails;
