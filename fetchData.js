e3d1ca1bf958b76cc2f9bcf06d4687ee6cccb376
Anwar Bahou <anwarbahou@fh-b85888.local>
2024-02-02 04:23:46 +0100

    the fetch function is used to call data from the provided link by the user

---
+++ fetchData.js
@@ -0,0 +1,14 @@
+
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
+module.exports = fetchData;
